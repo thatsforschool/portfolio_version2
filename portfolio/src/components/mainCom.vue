a94f765415e7f31ca8a458bf91ee1bab026aa65b
thatsforschool <93881287+thatsforschool@users.noreply.github.com>
2022-11-15 18:46:26 +0100

    created

---
+++ portfolio/src/components/mainCom.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{msg}}</div>
+  <main class="flex">
+    <article class="txt"></article>
+    <article class="img">
+      <img src="" alt="">
+    </article>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: "mainCom",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ portfolio/src/views/WorkView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <mainCom msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import mainCom from "@/components/mainCom.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    mainCom,
+  },
+};
+</script>
